177d0d5e9bcdcdc1879c41607f80f1d26bb8527c
aelgasser <abderraouf.elgasser@gmail.com>
2017-12-25 15:18:02 -0800

    Version 0.0.1 - It's Alive!

---
+++ src/Main.elm
@@ -0,0 +1,49 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Events exposing (..)
+import Html.Attributes exposing (..)
+
+
+type alias Model =
+    { quote : String
+    }
+
+
+type Msg
+    = GetQuote
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( Model "", Cmd.none )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        GetQuote ->
+            ( { model | quote = model.quote ++ "A quote: " }, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    div [ class "container" ]
+        [ h2 [ class "text-center" ] [ text "Chuck Norris Quotes" ]
+        , p [ class "text-center" ]
+            [ button [ class "btn btn-success", onClick GetQuote ] [ text "Grab a quote!" ]
+            ]
+        , blockquote []
+            [ p [] [ text model.quote ]
+            ]
+        ]
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = init
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        , view = view
+        }
