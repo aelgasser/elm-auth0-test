690a73109e73797d495c72dd9700bce05fbfdc74
aelgasser <abderraouf.elgasser@gmail.com>
2017-12-25 17:08:57 -0800

    Added the ability to register a new user

--- src/Main.elm
+++ src/Main.elm
@@ -4,21 +4,73 @@ import Html exposing (..)
 import Html.Events exposing (..)
 import Html.Attributes exposing (..)
 import Http
+import Json.Decode as Decode exposing (..)
+import Json.Encode as Encode exposing (..)
 
 
 type alias Model =
-    { quote : String
+    { username : String
+    , password : String
+    , token : String
+    , quote : String
+    , errorMsg : String
     }
 
 
 type Msg
     = GetQuote
     | FetchRandomQuoteCompleted (Result Http.Error String)
+    | SetUsername String
+    | SetPassword String
+    | ClickRegisterUser
+    | GetTokenCompleted (Result Http.Error String)
 
 
 api : String
 api =
     "http://localhost:3001/"
+
+
+registerUrl : String
+registerUrl =
+    api ++ "users"
+
+
+userEncoder : Model -> Encode.Value
+userEncoder model =
+    Encode.object
+        [ ( "username", Encode.string model.username )
+        , ( "password", Encode.string model.password )
+        ]
+
+
+authUser : Model -> String -> Http.Request String
+authUser model apiUrl =
+    let
+        body =
+            model |> userEncoder |> Http.jsonBody
+    in
+        Http.post apiUrl body tokenDecoder
+
+
+authUserCmd : Model -> String -> Cmd Msg
+authUserCmd model apiUrl =
+    Http.send GetTokenCompleted (authUser model apiUrl)
+
+
+getTokenCompleted : Model -> Result Http.Error String -> ( Model, Cmd Msg )
+getTokenCompleted model result =
+    case result of
+        Ok newToken ->
+            ( { model | token = newToken, password = "", errorMsg = "" } |> Debug.log "got new token", Cmd.none )
+
+        Err error ->
+            ( { model | errorMsg = (toString error) }, Cmd.none )
+
+
+tokenDecoder : Decoder String
+tokenDecoder =
+    Decode.field "access_token" Decode.string
 
 
 randomQuoteUrl : String
@@ -48,7 +100,7 @@ fetchRandomQuoteCompleted model result =
 
 init : ( Model, Cmd Msg )
 init =
-    ( Model "", fetchRandomQuoteCmd )
+    ( Model "" "" "" "" "", fetchRandomQuoteCmd )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -60,18 +112,81 @@ update msg model =
         FetchRandomQuoteCompleted result ->
             fetchRandomQuoteCompleted model result
 
+        SetUsername login ->
+            ( { model | username = login }, Cmd.none )
+
+        SetPassword pwd ->
+            ( { model | password = pwd }, Cmd.none )
+
+        ClickRegisterUser ->
+            ( model, authUserCmd model registerUrl )
+
+        GetTokenCompleted result ->
+            getTokenCompleted model result
+
 
 view : Model -> Html Msg
 view model =
-    div [ class "container" ]
-        [ h2 [ class "text-center" ] [ text "Chuck Norris Quotes" ]
-        , p [ class "text-center" ]
-            [ button [ class "btn btn-success", onClick GetQuote ] [ text "Grab a quote!" ]
+    let
+        loggedIn : Bool
+        loggedIn =
+            if String.length model.token > 0 then
+                True
+            else
+                False
+
+        authBoxView =
+            let
+                showError : String
+                showError =
+                    if String.isEmpty model.errorMsg then
+                        "hidden"
+                    else
+                        ""
+
+                greeting : String
+                greeting =
+                    "Hello, " ++ model.username ++ "!"
+            in
+                if loggedIn then
+                    div [ id "greeting" ]
+                        [ h3 [ class "text-center" ] [ text greeting ]
+                        , p [ class "text-center" ] [ text "You have super-secret access to protected quotes." ]
+                        ]
+                else
+                    div [ id "form" ]
+                        [ h2 [ class "text-center" ] [ text "Log In or Register" ]
+                        , p [ class "help-block" ] [ text "If you already have an account, please Log In. Otherwise, enter your desired username and password and Register. " ]
+                        , div [ class showError ]
+                            [ div [ class "alert alert-danger" ] [ text model.errorMsg ]
+                            ]
+                        , div [ class "form-group row" ]
+                            [ div [ class "col-md-offset-2 col-md-8" ]
+                                [ label [ for "username" ] [ text "Username:" ]
+                                , input [ id "username", type_ "text", class "form-control", Html.Attributes.value model.username, onInput SetUsername ] []
+                                ]
+                            ]
+                        , div [ class "form-group row" ]
+                            [ div [ class "col-md-offset-2 col-md-8" ]
+                                [ label [ for "password" ] [ text "Password:" ]
+                                , input [ id "password", type_ "text", class "form-control", Html.Attributes.value model.password, onInput SetPassword ] []
+                                ]
+                            ]
+                        , div [ class "text-center" ]
+                            [ button [ class "btn btn-link", onClick ClickRegisterUser ] [ text "Register" ] ]
+                        ]
+    in
+        div [ class "container" ]
+            [ h2 [ class "text-center" ] [ text "Chuck Norris Quotes" ]
+            , p [ class "text-center" ]
+                [ button [ class "btn btn-success", onClick GetQuote ] [ text "Grab a quote!" ]
+                ]
+            , blockquote []
+                [ p [] [ text model.quote ]
+                ]
+            , div [ class "jumbotron text-left" ]
+                [ authBoxView ]
             ]
-        , blockquote []
-            [ p [] [ text model.quote ]
-            ]
-        ]
 
 
 main : Program Never Model Msg
